a502484bf05cff86af017477c9bbdb870eafeb9d
mgarusi101@gmail.com <Aknvvnkl1>
2018-11-18 16:06:48 +0200

    fix bugs

--- ex_2/src/Coords/MyCoords.java
+++ ex_2/src/Coords/MyCoords.java
@@ -7,23 +7,22 @@ import java.util.Map;
 import com.sun.javafx.geom.AreaOp.AddOp;
 import com.sun.javafx.scene.paint.GradientUtils.Point;
 
-public class coords_convertor {/**
+public class MyCoords {/**
 
 		/** computes a new point which is the gps point transformed by a 3D vector (in meters)*/
 	public static Point3D add(Point3D gps, Point3D local_vector_in_meter) {
 
 		Point3D meter= gps.GpsToMeter();
-//		System.out.println(utmGps.toString());
+		//		System.out.println(utmGps.toString());
 		meter.add(local_vector_in_meter);
-//		System.out.println(utmGps.toString());
+		//		System.out.println(utmGps.toString());
 		Point3D outputGps = meter.meterToGps();
-//		System.out.println(outputGps.toString());
+		//		System.out.println(outputGps.toString());
 
 		return outputGps;
 
 	}
-	
-	//	public double distance3d(Point3D gps0, Point3D gps1) {
+
 
 
 	public double distance3d(Point3D gps0, Point3D gps1) {
@@ -35,13 +34,9 @@ public class coords_convertor {/**
 
 	}
 	/** computes the 3D vector (in meters) between two gps like points */
-
-
-	/** computes the 3D vector (in meters) between two gps like points */
 	public Point3D vector3D(Point3D gps0, Point3D gps1) {
 
 		Point3D vector = new Point3D(gps1.x()-gps0.x(), gps1.y()-gps0.y(), gps1.z()-gps0.z());
-
 		return vector;
 	}
 	/** computes the polar representation of the 3D vector be gps0-->gps1 
@@ -49,15 +44,9 @@ public class coords_convertor {/**
 	public  double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1) {
 
 		double [] ans = {0,0,0};
-
 		ans[0] = gps0.north_angle(gps1);
 		ans[1] = gps0.up_angle(gps1); 
-		//				(gps1.z()-gps0.z())/(Math.sqrt((Math.pow((gps1.x()-gps0.x()), 2)
-		//				+(Math.pow((gps1.y()-gps0.y()), 2)))));
-
 		ans[2] =(distance3d(gps0, gps1));
-
-
 		return ans;
 
 	}
