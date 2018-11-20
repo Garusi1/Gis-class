d3bdbe3192b974f161c620f38fec599f41a36876
mgarusi101@gmail.com <Aknvvnkl1>
2018-11-20 11:49:16 +0200

    fix bugs

---
+++ ex_2/src/File_format/main.java
@@ -0,0 +1,29 @@
+package File_format;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import jdk.internal.org.objectweb.asm.commons.TryCatchBlockSorter;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+			String fileName = "cc.csv";
+		File file = new File (fileName);
+		
+		file.
+		
+		
+		try {
+			Scanner input = new Scanner(file);
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+}
