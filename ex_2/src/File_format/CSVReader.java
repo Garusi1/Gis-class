162248bd1d91e28e691bb9aac5e62e0d75a17309
naor9 <naor9@DESKTOP-FCQ5HQM>
2018-11-29 16:22:57 +0200

    build the function

--- ex_2/src/File_format/CSVReader.java
+++ ex_2/src/File_format/CSVReader.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import GIS1.MyGisElement;
+import GIS1.MyGisLayer;
 import Geom.Point3D;
 
 public class CSVReader {
@@ -11,30 +13,33 @@ public class CSVReader {
     public static void main(String[] args) 
     {
     	
-    	System.out.println("fbhdsfghdsj");
-        String csvFile = "WigleWifi_20171203085618.csv";
+
+        String csvFile = "WigleWifi_20171201110209.csv";
         String line = "";
         String cvsSplitBy = ",";
-int i=0;
+        MyGisLayer L= new MyGisLayer();
+
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) 
         {
-        	
-        }
+        	line = br.readLine();
             while ((line = br.readLine()) != null) 
             {
-//                Point3D p = (Double.parseDouble(userInfo[0]),(Double.parseDouble(userInfo[1]))
-            	
-            	String[] userInfo = line.split(cvsSplitBy);
-
-                System.out.println("x," + userInfo[0] + "y" + userInfo[1] +
-                		" Phone: " + userInfo[2] + " Country: " + userInfo[3] );
+                String[] userInfo = line.split(cvsSplitBy);
+                
+              Point3D p = new Point3D ((Double.parseDouble(userInfo[0])),(Double.parseDouble(userInfo[1])),(Double.parseDouble(userInfo[2])));
+              MyGisElement pp = new MyGisElement(p, userInfo[3],userInfo[5], userInfo[6], "", Long.parseLong(userInfo[4]));
+              L.add(pp);
+              System.out.println();
+             // System.out.println(L.isEmpty()); 
+              ;
+                System.out.println("X: " + userInfo[0] + " , Y: " + userInfo[1] +
+                		" z: " + userInfo[2] + " ,timing: " + userInfo[3]+ " ,UTC zone: "
+                				+ ""+ userInfo[4]+ " ,data: "+ userInfo[5]+ " ,description: "+ userInfo[6]);
             }
 
         } catch (IOException e) 
         {
             e.printStackTrace();
-//        }
-
+        }
     }
-
 }
